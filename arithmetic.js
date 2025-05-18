6e0d73c42d1b9d66785a0f67de5d96afbbfd2659
rajaumarmehmood <rajaumarmehmood2617@gmail.com>
2025-05-18 19:42:20 +0500

    checking git

---
+++ arithmetic.js
@@ -0,0 +1,17 @@
+// Basic arithmetic operations
+const add = (a, b) => a + b;
+const subtract = (a, b) => a - b;
+const multiply = (a, b) => a * b;
+const divide = (a, b) => {
+    if (b === 0) {
+        throw new Error('Division by zero is not allowed');
+    }
+    return a / b;
+};
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide
+}; 
